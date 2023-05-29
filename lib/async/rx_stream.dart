dd351248a06278fa5c14a52f7964bac21aabd39a
Daniel Otavio Todt <danieltodt@gmail.com>
2023-05-29 18:23:56 -0300

    doc rx stream api doc

--- lib/async/rx_stream.dart
+++ lib/async/rx_stream.dart
@@ -1,19 +1,49 @@
 part of '../asp.dart';
 
-enum StreamStatus { waiting, active, done, error }
+/// RxStream status values.
+enum StreamStatus {
+  /// initial status
+  waiting,
 
+  /// on data status
+  active,
+
+  /// on finish status
+  done,
+
+  /// on error status
+  error,
+}
+
+/// The RxStream is the reactive wrapper around a StreamSubscription.
+/// You can use it to show the UI under various states of a Stream,
+/// from waiting to active, done or rejected.
+///
+/// The status, data and error fields of an RxStream are observable and can be consumed on the UI.
 class RxStream<T> extends Stream<T> {
   late final StreamSubscription _sub;
 
+  /// Creates a [RxStream] from a [stream].
+  /// {@tool snippet}
+  /// ```dart
+  /// final rxStream = RxStream.of(stream);
+  /// ```
+  /// {@end-tool}
   static RxStream<T> of<T>(Stream<T> stream) => RxStream._(stream);
 
   final Atom<StreamStatus> _status = Atom<StreamStatus>(StreamStatus.waiting);
 
   late final Atom<T?> _result;
+
+  /// The current value, that may be null
   T? get data => _result.value;
+
+  /// The current status
   StreamStatus get status => _status.value;
 
   final Atom _error = Atom(null);
+
+  /// The current error
   dynamic get error => _error.value;
 
   late final Stream<T> _stream;
@@ -51,6 +81,7 @@ class RxStream<T> extends Stream<T> {
     );
   }
 
+  /// Closes the origin stream
   @mustCallSuper
   Future close() async {
     await _sub.cancel();
